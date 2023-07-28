c4ebdce22fabed034e489ffbb5b25b98c82448d3
MitsuhaYuki <mitsuhayuki@qq.com>
2023-07-28 10:39:08 +0800

    fix:  Replace the loading animation with animejs.

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -24,6 +24,7 @@ const App: FC = () => {
     document.querySelector('#root-loading-screen')?.classList.add('elem-fade-out')
     setTimeout(() => {
       document.querySelector('#root-loading-screen')?.classList.add('elem-hidden')
+      window.postMessage({ type: 'STOP_ANIME' })
     }, 500)
   })
 
