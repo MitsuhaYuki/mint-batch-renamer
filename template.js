ea17105a351a78dccdfb193574896d2e3a07f8b1
MitsuhaYuki <mitsuhayuki@qq.com>
2023-07-28 10:41:19 +0800

    fix: Allow window resize and template adjustment

--- template.js
+++ template.js
@@ -1,6 +1,9 @@
 /*
- * Use this file to generate all type of template files in this project
- * In project root folder, run command `node template.js` to generate all templates
+ * @Author: MitsuhaYuki
+ * @Date: 2023-07-12 16:08:39
+ * @LastEditTime: 2023-07-27 16:23:04
+ * @LastEditors: MitsuhaYuki
+ * @Description: Use this file to generate all type of template files in this project, use command `pnpm build:template` to generate all templates
  */
 import fs from 'fs'
 
@@ -41,44 +44,45 @@ const tExternalFilter = {
       default: ''
     }],
     func: btoa(encodeURIComponent(((sysArgs, extra) => {
-      const DOCUMENT = `
-        过滤器接受两个参数, 分别是sysArgs和extra, 其中:
-          1. sysArgs:
-            内部传入的数据结构, 当过滤器的作用域为“单个文件”时, 该参数的结构为:
-              type IFileNameScopeFilterArgs = {
+      `DOC_START===>
+      过滤器接受两个参数, 分别是sysArgs和extra, 其中:
+        1. sysArgs:
+          内部传入的数据结构, 当过滤器的作用域为“单个文件”时, 该参数的结构为:
+            type IFileNameScopeFilterArgs = {
+              /** 文件全名 */
+              fullName: string
+              /** 文件名 */
+              fileName: string
+              /** 文件扩展名 */
+              extName: string
+            }
+          当过滤器的作用域为“文件列表”时, 该参数的结构为:
+            type IFileListScopeFilterArgs = {
+              /** 所有文件列表 */
+              fileList: {
                 /** 文件全名 */
-                fullName: string
+                full_name: string
                 /** 文件名 */
-                fileName: string
+                name: string
+                /** 文件大小 */
+                size: number
                 /** 文件扩展名 */
-                extName: string
-              }
-            当过滤器的作用域为“文件列表”时, 该参数的结构为:
-              type IFileListScopeFilterArgs = {
-                /** 所有文件列表 */
-                fileList: {
-                  /** 文件全名 */
-                  full_name: string
-                  /** 文件名 */
-                  name: string
-                  /** 文件大小 */
-                  size: number
-                  /** 文件扩展名 */
-                  extension: string
-                  /** 文件路径 */
-                  path: string
-                }[]
-              }
-          2. extra
-            外部参数, 类型为Record<string,any>
-            在参数列表中定义的参数, 会使用id:value的形式传入extra中, 使用extra.[参数ID]的形式可以访问它
-        过滤器的返回值:
-          当过滤器的作用域为“单个文件”时, 此函数返回一个boolean值代表是否应该保留此文件
-          当过滤器的作用域为“文件列表”时, 此函数返回一个文件列表, 代表应该保留的文件, 注意: 与此同时文件列表的顺序也会被保留, 在此情况下可以用作排序函数使用
-        代码示例:
-          在当前的过滤器例子中, 作用域为“单个文件”, 参数列表定义了两个参数, 分别是filter_range和contains_text
-          以下代码根据filter_range的值决定该过滤文件名的哪些部分, 使用contains_text的值进行匹配
-          最终返回一个boolean值告诉程序是否应该保留此文件`
+                extension: string
+                /** 文件路径 */
+                path: string
+              }[]
+            }
+        2. extra
+          外部参数, 类型为Record<string,any>
+          在参数列表中定义的参数, 会使用id:value的形式传入extra中, 使用extra.[参数ID]的形式可以访问它
+      过滤器的返回值:
+        当过滤器的作用域为“单个文件”时, 此函数返回一个boolean值代表是否应该保留此文件
+        当过滤器的作用域为“文件列表”时, 此函数返回一个文件列表, 代表应该保留的文件, 注意: 与此同时文件列表的顺序也会被保留, 在此情况下可以用作排序函数使用
+      代码示例:
+        在当前的过滤器例子中, 作用域为“单个文件”, 参数列表定义了两个参数, 分别是filter_range和contains_text
+        以下代码根据filter_range的值决定该过滤文件名的哪些部分, 使用contains_text的值进行匹配
+        最终返回一个boolean值告诉程序是否应该保留此文件
+      <===DOC_END`
       const filterRange = extra?.filter_range ?? 'file'
       switch (filterRange) {
         case 'file':
@@ -125,7 +129,7 @@ const tExternalRenamer = {
       default: ''
     }],
     func: btoa(encodeURIComponent((async (sysArgs, extra) => {
-      const DOCUMENT = `
+      `DOC_START===>
       重命名器接受两个参数, 分别是sysArgs和extra, 其中:
         1. sysArgs:
           内部传入的数据结构, 该参数的结构为:
@@ -178,7 +182,8 @@ const tExternalRenamer = {
       代码示例:
         在当前的重命名器例子中, 作用域为“单个文件”, 参数列表定义了两个参数, 分别是filter_range和contains_text
         以下代码根据filter_range的值决定该过滤文件名的哪些部分, 使用contains_text的值进行匹配
-        最终返回一个boolean值告诉程序是否应该保留此文件`
+        最终返回一个boolean值告诉程序是否应该保留此文件
+      <===DOC_END`
       let curName = `${sysArgs.fileItem.rename_name ?? sysArgs.fileItem.name}`
       let curExt = `${sysArgs.fileItem.rename_extension ?? sysArgs.fileItem.extension}`
 
