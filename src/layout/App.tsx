0be08089cdd17b46a5abab8ee78203dc1da77443
MitsuhaYuki <mitsuhayuki@qq.com>
2023-10-06 03:56:15 +0800

    fix: Adjust window parameters.

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -1,6 +1,6 @@
 import MainPage from './main'
 import zhCN from 'antd/locale/zh_CN'
-import { ConfigProvider } from 'antd'
+import { App, ConfigProvider } from 'antd'
 import { ConsoleContext, consoleInitState, consoleReducer } from '@/context/console'
 import { FC, useReducer } from 'react'
 import { GlobalContext, globalInitState, globalReducer } from '@/context/global'
@@ -10,7 +10,7 @@ import { useMount } from 'ahooks'
 import './App.scss'
 
 const baseCls = 'app'
-const App: FC = () => {
+const Content: FC = () => {
   // initialize all context
   const [globalState, globalDispatch] = useReducer(globalReducer, globalInitState)
   const [consoleState, consoleDispatch] = useReducer(consoleReducer, consoleInitState)
@@ -32,15 +32,17 @@ const App: FC = () => {
     <ConfigProvider autoInsertSpaceInButton={false} locale={zhCN}>
       <GlobalContext.Provider value={{ state: globalState, dispatch: globalDispatch }}>
         <ConsoleContext.Provider value={{ state: consoleState, dispatch: consoleDispatch }}>
-          <div className={baseCls}>
-            <MainPage />
-          </div>
+          <App>
+            <div className={baseCls}>
+              <MainPage />
+            </div>
+          </App>
         </ConsoleContext.Provider>
       </GlobalContext.Provider>
     </ConfigProvider>
   )
 }
 
-App.defaultProps = {}
-App.displayName = baseCls
-export default App
+Content.defaultProps = {}
+Content.displayName = baseCls
+export { Content as App }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 // import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './layout/App'
+import { App } from './layout/App'
 import 'antd/dist/reset.css'
 import './main.css'
 
